14497d94dd76a67a931a50adfe59b4ecf0509597
Pedro Rubin <prubincosta@gmail.com>
2021-11-06 20:42:20 -0300

    first commit

---
+++ R/baixar_pnad_covid.R
@@ -0,0 +1,46 @@
+#' Baixa e descompacta os microdados da PNAD COVID
+#'
+#' Baixa e descompacta os microdados da PNAD COVID (é preciso acesso a internet)
+#' @param ano Mês da PNAD COVID (número)
+#' @param caminho_pasta A pasta (o caminho para a pasta) na qual os arquivos serão guardados (se não existir, a pasta será criada)
+#' @return Os arquivos de microdados (txt) na pasta designada
+#' @examples baixar_pnad_covid(mes = 5, caminho_pasta = "./microdata");
+#' @export
+
+
+baixar_pnad_covid <- function(mes, caminho_pasta){
+
+  mes <- as.character(mes)
+  mes <- str_pad(mes, 2, "left", "0")
+
+  if(ano == "05"){
+    url <- "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_PNAD_COVID19/Microdados/Dados/PNAD_COVID_052020.zip"
+  }
+  else if(ano == "06"){
+    url <- "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_PNAD_COVID19/Microdados/Dados/PNAD_COVID_062020.zip"
+  }
+  else if(ano == "07"){
+    url <- "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_PNAD_COVID19/Microdados/Dados/PNAD_COVID_072020.zip"
+  }
+  else if(ano == "08"){
+    url <- "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_PNAD_COVID19/Microdados/Dados/PNAD_COVID_082020.zip"
+  }
+  else if(ano == "09"){
+    url <- "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_PNAD_COVID19/Microdados/Dados/PNAD_COVID_092020.zip"
+  }
+  else if(ano == "10"){
+    url <- "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_PNAD_COVID19/Microdados/Dados/PNAD_COVID_102020.zip"
+  }
+  else if(ano == "11"){
+    url <- "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_PNAD_COVID19/Microdados/Dados/PNAD_COVID_112020.zip"
+  }
+
+  tempFile <- tempfile()
+
+  download.file(url,tempFile,quiet=TRUE,mode="wb")
+
+  unzip(file.path(tempFile), exdir = caminho_pasta)
+
+}
+
+
